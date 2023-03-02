93c6dd7ff21a8868defa2605ac314cb92d273527
Leonardo Alves <leonardo.alves779@gmail.com>
2023-03-02 12:17:04 -0300

    including sweet alert API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { FiSearch } from 'react-icons/fi'
 import './styles.css'
 import { api } from './services/api'
 
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+const MySwal = withReactContent(Swal)
+
 export function App() {
 
   const [input, setInput] = useState('')
@@ -12,17 +17,29 @@ export function App() {
   async function handleSearch(){
 
     if(input === ''){
-      alert("Por favor, digite algum CEP!")
+      
+      MySwal.fire({
+        title: "Não foi possível fazer a busca",
+        text: "Por favor, digite algum CEP.",
+        icon: "error"
+      })
+
       return
     }
 
     try{
       const response = await api.get(`${input}/json`)
       setCep(response.data)
+
       setInput('')
     }
     catch{
-      alert("Oops... Erro ao buscar.")
+      MySwal.fire({
+        title: "Oops... Erro ao buscar",
+        text: "Por favor, digite um CEP válido.",
+        icon: "error"
+      })
+
       setInput("")
     }
 
